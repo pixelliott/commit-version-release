182b503a3555dc78ef5229b6413eed61f5d94660
Joseph Elliott <josephelliott@gmx.com>
2022-11-17 20:37:05 +0000

    Updated inputs

--- CommitVersionRelease/Models/ActionInputs.cs
+++ CommitVersionRelease/Models/ActionInputs.cs
@@ -5,12 +5,12 @@ public class ActionInputs
     [Option('j', "package-json-path", Required = true, HelpText = "The package.json path. e.g. `package.json`.")]
     public string PackageJsonPath { get; set; }
 
-    [Option('r', "repo", Required = true, HelpText = "The repository name in the format owner/repo. Assign from `github.repository`.")]
+    [Option('u', "repo", Required = true, HelpText = "The repository name in the format owner/repo. Assign from `github.repository`.")]
     public string Repo { get; set; }
 
-    [Option('c', "commit-reference", Required = true, HelpText = "The reference to the commit. Assign from `github.event.head_commit.ref`")]
+    [Option('e', "commit-reference", Required = true, HelpText = "The reference to the commit. Assign from `github.event.head_commit.ref`")]
     public string CommitReference { get; set; }
 
-    [Option('t', "github-token", Required = true, HelpText = "Github token. Assign from `github.token`.")]
+    [Option('g', "github-token", Required = true, HelpText = "Github token. Assign from `github.token`.")]
     public string GitHubToken { get; set; }
 }
